9e49602ab7f5bcaf7fdeed00885e3a26b5740f2f
Adam Griffin <ag@agriffin.me>
2025-08-25 15:53:51 -0700

    Added users field to CompanyType

--- app/graphql/types/company_type.rb
+++ app/graphql/types/company_type.rb
@@ -4,5 +4,10 @@ module Types
   class CompanyType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
+    field :users, [ Types::UserType ], null: true
+
+    def users
+      object.users
+    end
   end
 end
