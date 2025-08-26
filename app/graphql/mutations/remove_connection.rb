b0ac434559de56f25e6b8462c8c404c82df4e0dd
Adam Griffin <ag@agriffin.me>
2025-08-25 18:11:43 -0700

    Added RemoveConnection

---
+++ app/graphql/mutations/remove_connection.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Mutations
+  class RemoveConnection < Mutations::BaseMutation
+    argument :user_a_id, ID, required: true
+    argument :user_b_id, ID, required: true
+
+    field :success, Boolean, null: false
+    field :errors, [ String ], null: false
+
+    def resolve(user_a_id:, user_b_id:)
+      begin
+        Connection.remove_connection(user_a_id, user_b_id)
+        { success: true, errors: [] }
+      rescue => e
+        { success: false, errors: [ e.message ] }
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,5 +5,6 @@ module Types
     field :send_connection_request, mutation: Mutations::SendConnectionRequest
   field :accept_connection_request, mutation: Mutations::AcceptConnectionRequest
   field :decline_connection_request, mutation: Mutations::DeclineConnectionRequest
+  field :remove_connection, mutation: Mutations::RemoveConnection
   end
 end
