0c81baa87270be6a571a9396113b466431bfcba9
Adam Griffin <ag@agriffin.me>
2025-08-26 19:06:08 -0700

    Added ConnectionType

---
+++ app/graphql/types/connection_type.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Types
+  class ConnectionType < Types::BaseObject
+    field :id, ID, null: false
+    field :requester_id, Integer, null: false
+    field :recipient_id, Integer, null: false
+    field :status, String, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :requester, Types::UserType, null: false do
+      description "The user who sent the connection request"
+    end
+    field :recipient, Types::UserType, null: false do
+      description "The user who received the connection request"
+    end
+
+    def requester
+      object.requester
+    end
+
+    def recipient
+      object.recipient
+    end
+  end
+end
