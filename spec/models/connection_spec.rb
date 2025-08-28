5e223eedcb0483bc6bd51e4cdc21c3c7eafcf8b3
Adam Griffin <ag@agriffin.me>
2025-08-27 20:44:23 -0700

    Added test for accept_request

--- spec/models/connection_spec.rb
+++ spec/models/connection_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Connection, type: :model do
   let(:house) { create(:house) }
   let(:wilson)  { create(:wilson) }
+  let(:cameron) { create(:cameron) }
 
   describe "#send_connection_request" do
     it "creates a new connection with pending status" do
@@ -27,6 +28,21 @@ RSpec.describe Connection, type: :model do
       expect {
         Connection.send_connection_request(wilson.id, house.id)
       }.to raise_error("Connection already exists")
+    end
+  end
+
+  describe "#accept_request" do
+    it "accepts a pending connection request" do
+      pending_connection = create(:connection, requester: house, recipient: wilson, status: "pending")
+      Connection.accept_request(pending_connection.id)
+      expect(pending_connection.reload.status).to eq("accepted")
+    end
+
+    it "raises an error if the request is not pending" do
+      accepted_connection = create(:connection, requester: house, recipient: cameron, status: "accepted")
+      expect {
+        Connection.accept_request(accepted_connection.id)
+      }.to raise_error("Request is not pending")
     end
   end
 end
