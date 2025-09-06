5f13105a2c745e9aad4c75ae0b83a1303e9100d3
Adam Griffin <ag@agriffin.me>
2025-09-06 16:48:31 -0700

    Formatting

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,16 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE
     }
   },
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
-})
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/test-utils", "@nuxt/ui"]
+});
