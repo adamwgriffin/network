0b7b308f836b4529b3571a88672361e66eb41b22
Adam Griffin <ag@agriffin.me>
2025-09-07 22:51:16 -0700

    Added FeedPostButton

---
+++ client/app/components/FeedPostButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ iconName: string }>();
+</script>
+
+<template>
+  <button class="flex items-center gap-1 cursor-pointer">
+    <UIcon :name="iconName" class="size-5" /> <slot />
+  </button>
+</template>
