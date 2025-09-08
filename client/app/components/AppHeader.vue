3845b0cd37583c4308c763d45d6fb8d4202ea1dd
Adam Griffin <ag@agriffin.me>
2025-09-07 22:50:32 -0700

    Added Header component

---
+++ client/app/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="flex items-end gap-12 p-6">
+    <NuxtLink to="/">
+      <h1 class="text-3xl font-bold">Network 🧑‍⚕️👩‍⚕️</h1>
+      <p>The ultimate doctors network&trade;</p>
+    </NuxtLink>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink> |
+      <NuxtLink to="/mynetwork">My Network</NuxtLink>
+    </nav>
+  </header>
+</template>
