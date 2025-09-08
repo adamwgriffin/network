ec80c4290f0ab90dc4702acb46c10a35be9172a1
Adam Griffin <ag@agriffin.me>
2025-09-07 22:52:19 -0700

    Added dummy feed to index page

--- client/app/pages/index.vue
+++ client/app/pages/index.vue
@@ -1,31 +1,33 @@
-<script lang="ts" setup>
-const queryVariables: GetUsersQueryVariables = { first: 5 };
-const { data, error } = await useAsyncQuery<GetUsersQuery>(
-  GetUsersDocument,
-  queryVariables
-);
-// The filter function with "is" defines a type predicate in order to assert
-// that nothing is null in the data. This way we don't have to constantly check
-// it inside the template
-const edges =
-  data.value?.users.edges?.filter(
-    (e): e is UserEdge & { node: User } => !!e && !!e.node
-  ) ?? [];
-</script>
-
 <template>
-  <main>
-    <h1 class="text-2xl font-bold">Network</h1>
-    <p>The ultimate doctors network</p>
-    <ul>
-      <li v-for="{ node } in edges" :key="node.id">
-        <NuxtLink :to="`/users/${node.id}`">
-          {{ getFullNameAndCredentials(node) }}
-        </NuxtLink>
+  <div>
+    <ul class="space-y-4 w-140">
+      <li>
+        <FeedPost />
+      </li>
+      <li>
+        <FeedPost />
+      </li>
+      <li>
+        <FeedPost />
+      </li>
+      <li>
+        <FeedPost />
+      </li>
+      <li>
+        <FeedPost />
+      </li>
+      <li>
+        <FeedPost />
+      </li>
+      <li>
+        <FeedPost />
+      </li>
+      <li>
+        <FeedPost />
+      </li>
+      <li>
+        <FeedPost />
       </li>
     </ul>
-    <p v-if="error">
-      {{ error }}
-    </p>
-  </main>
+  </div>
 </template>
