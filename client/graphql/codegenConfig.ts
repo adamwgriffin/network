f05eb43eec826fa8d07c97cae08b34dcad7e5b6d
Adam Griffin <ag@agriffin.me>
2025-09-07 20:25:54 -0700

    Updated GraphQL Codegen config
    It was really hard to get this working right! Had to look up a lot of things in the docs in order to figure out how to get stricter types that work with what Nuxt is doing. Also had to add a bunch of config for vue-appolo to import things from the correct places for Nuxt 3. Move a few things around to get it more organized.

---
+++ client/graphql/codegenConfig.ts
@@ -0,0 +1,38 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "http://localhost:4000/graphql",
+  documents: ["./graphql/queries/**/*.{gql,graphql}"],
+  generates: {
+    "./app/utils/graphqlCodegen.ts": {
+      plugins: ["typescript", "typescript-operations", "typescript-vue-apollo"]
+    }
+  },
+  config: {
+    // Will use import type {} rather than import {} when importing only types.
+    // This gives compatibility with TypeScript's "importsNotUsedAsValues":
+    // "error" option
+    useTypeImports: true,
+    // This will cause the generator to avoid using TypeScript optionals (?) on
+    // types, so the following definition: type A { myField: String } will
+    // output myField: Maybe<string> instead of myField?: Maybe<string>.
+    avoidOptionals: true,
+    // If scalars are found in the schema that are not defined in scalars an
+    // error will be thrown during codegen.
+    strictScalars: true,
+    // Define custom scalars which would be found in the schema so they're
+    // output as the correct types
+    scalars: {
+      ISO8601DateTime: "string"
+    },
+    // This is all for typescript-vue-apollo plugin. The main concern is to
+    // configure the plugin to import from the correct places for Vue 3, which
+    // it does not do by default.
+    vueCompositionApiImportFrom: "vue",
+    withCompositionFunctions: true,
+    vueApolloComposableImportFrom: "@vue/apollo-composable",
+    addDocBlocks: true
+  }
+};
+
+export default config;
