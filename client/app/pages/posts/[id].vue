087501e8d06a94a03e42b6ccdfddbce12ddff7f3
Adam Griffin <ag@agriffin.me>
2025-09-11 15:32:03 -0700

    Added post page

---
+++ client/app/pages/posts/[id].vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const route = useRoute();
+const { data } = await useAsyncQuery<GetPostQuery>(GetPostDocument, {
+  id: getRouteParam(route.params.id)
+} satisfies GetPostQueryVariables);
+</script>
+
+<template>
+  <div v-if="data?.post">
+    <div class="pb-6">
+      {{ getFullNameAndCredentials(data.post.user) }}
+    </div>
+    <pre class="whitespace-pre-line font-sans">
+      {{ data.post.body }}
+    </pre>
+  </div>
+  <div v-else>
+    <p>Nothing to see here</p>
+  </div>
+</template>
