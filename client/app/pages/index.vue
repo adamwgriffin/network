0b0d7838bb77943d71abf4bfde56de47f55bb39c
Adam Griffin <ag@agriffin.me>
2025-09-13 15:14:55 -0700

    Added load more results to network feed
    Using useIntersectionObserver to check when user is about to scroll to the end then loading more results with fetchMore

--- client/app/pages/index.vue
+++ client/app/pages/index.vue
@@ -1,6 +1,54 @@
 <script lang="ts" setup>
 const queryVariables: GetPostsQueryVariables = { first: 4 };
-const { result } = useGetPostsQuery(queryVariables);
+const { result, fetchMore } = useGetPostsQuery(queryVariables);
+
+function loadMore() {
+  fetchMore({
+    variables: {
+      ...queryVariables,
+      after: result.value?.posts.pageInfo.endCursor
+    } satisfies GetPostsQueryVariables,
+    updateQuery: (previousResult, { fetchMoreResult }) => {
+      const newEdges = fetchMoreResult.posts.edges ?? [];
+      const pageInfo = fetchMoreResult.posts.pageInfo;
+
+      if (newEdges.length) {
+        const newResult: GetPostsQuery = {
+          ...previousResult,
+          ...{
+            posts: {
+              // Concat edges
+              edges: [...(previousResult.posts.edges ?? []), ...newEdges],
+              // Override with new pageInfo
+              pageInfo
+            }
+          }
+        };
+
+        return newResult;
+      }
+
+      return previousResult;
+    }
+  });
+}
+
+const sentinel = ref<HTMLElement | null>(null);
+
+useIntersectionObserver(
+  sentinel,
+  ([entry]) => {
+    if (entry?.isIntersecting && result.value?.posts.pageInfo.hasNextPage) {
+      loadMore();
+    }
+  },
+  {
+    // The element to test for intersection. Using null means use viewport.
+    root: null,
+    // Trigger when sentinel is halfway to becoming visible
+    threshold: 0.5
+  }
+);
 </script>
 
 <template>
@@ -9,12 +57,13 @@ const { result } = useGetPostsQuery(queryVariables);
       <li v-for="edge in result.posts.edges" :key="edge?.node?.id">
         <FeedPost
           v-if="edge?.node?.user && edge?.node?.body"
-          :id="edge?.node.id"
+          :id="edge.node.id"
           :author="getFullNameAndCredentials(edge.node.user)"
           :body="edge.node.body"
           :author-slug="edge.node.user.slug"
         />
       </li>
+      <li ref="sentinel" />
     </ul>
   </div>
 </template>
