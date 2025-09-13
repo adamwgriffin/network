31dfa4bc0b0108f3da33c635f3504b62958d2cf4
Adam Griffin <ag@agriffin.me>
2025-09-12 18:33:31 -0700

    Refactored post
    Also added dropdown and copy link feature

--- client/app/components/FeedPost.vue
+++ client/app/components/FeedPost.vue
@@ -1,28 +1,64 @@
 <script lang="ts" setup>
-defineProps<{ body: string; author: string; authorSlug: string }>();
+import type { DropdownMenuItem } from "@nuxt/ui";
+
+const props = defineProps<{
+  id: string;
+  body: string;
+  author: string;
+  authorSlug: string;
+}>();
+
+const items = ref<DropdownMenuItem[]>([
+  {
+    label: "Copy link to post",
+    icon: "lucide:link",
+    async onSelect() {
+      await navigator.clipboard.writeText(
+        `${window.location.origin}/posts/${props.id}`
+      );
+    }
+  }
+]);
 </script>
 
 <template>
-  <UCard>
-    <template #header>
-      <h3>
+  <div class="flex flex-col gap-y-4 rounded-lg p-4 border border-blue-300">
+    <div
+      class="flex items-center justify-between pb-4 border-b border-blue-300"
+    >
+      <div>Suggested</div>
+      <UDropdownMenu
+        :items="items"
+        :content="{
+          align: 'end',
+          side: 'bottom'
+        }"
+      >
+        <UButton
+          icon="lucide:ellipsis"
+          color="neutral"
+          variant="ghost"
+          class="cursor-pointer rounded-full"
+        />
+      </UDropdownMenu>
+    </div>
+    <div>
+      <h3 class="font-semibold">
         <NuxtLink :to="`/users/${authorSlug}`">
           {{ author }}
         </NuxtLink>
       </h3>
-    </template>
+    </div>
     <p>
       {{ body }}
     </p>
-    <template #footer>
-      <div class="flex items-center gap-6">
-        <FeedPostButton icon-name="lucide:thumbs-up">Like</FeedPostButton>
-        <FeedPostButton icon-name="lucide:message-square-text">
-          Comment
-        </FeedPostButton>
-        <FeedPostButton icon-name="lucide:repeat-2">Repost</FeedPostButton>
-        <FeedPostButton icon-name="lucide:send">Send</FeedPostButton>
-      </div>
-    </template>
-  </UCard>
+    <div class="flex items-center gap-6 pt-4 border-t border-blue-300">
+      <FeedPostButton icon-name="lucide:thumbs-up">Like</FeedPostButton>
+      <FeedPostButton icon-name="lucide:message-square-text">
+        Comment
+      </FeedPostButton>
+      <FeedPostButton icon-name="lucide:repeat-2">Repost</FeedPostButton>
+      <FeedPostButton icon-name="lucide:send">Send</FeedPostButton>
+    </div>
+  </div>
 </template>
--- client/app/pages/index.vue
+++ client/app/pages/index.vue
@@ -9,6 +9,7 @@ const { result } = useGetPostsQuery(queryVariables);
       <li v-for="edge in result.posts.edges" :key="edge?.node?.id">
         <FeedPost
           v-if="edge?.node?.user && edge?.node?.body"
+          :id="edge?.node.id"
           :author="getFullNameAndCredentials(edge.node.user)"
           :body="edge.node.body"
           :author-slug="edge.node.user.slug"
