51dbde0616651d2baef6396e493462080c0809a5
Adam Griffin <ag@agriffin.me>
2025-09-15 20:01:01 -0700

    Changed structure of FeedPost
    This seems more semantic

--- client/app/components/FeedPost.vue
+++ client/app/components/FeedPost.vue
@@ -35,11 +35,8 @@ const items = ref<DropdownMenuItem[]>([
 </script>
 
 <template>
-  <div class="flex flex-col gap-y-4 rounded-lg p-4 border border-blue-300">
-    <div
-      class="flex items-center justify-between pb-4 border-b border-blue-300"
-    >
-      <div class="text-sm">Suggested</div>
+  <article class="flex flex-col gap-y-4 rounded-lg p-4 border border-blue-300">
+    <header class="flex items-center justify-end pb-4 border-b border-blue-300">
       <UDropdownMenu
         :items="items"
         :content="{
@@ -54,24 +51,22 @@ const items = ref<DropdownMenuItem[]>([
           class="cursor-pointer rounded-full"
         />
       </UDropdownMenu>
-    </div>
-    <div>
-      <h3 class="font-semibold">
-        <NuxtLink :to="`/users/${authorSlug}`" class="text-secondary text-sm">
-          {{ author }}
-        </NuxtLink>
-      </h3>
-    </div>
+    </header>
+    <h3 class="font-semibold">
+      <NuxtLink :to="`/users/${authorSlug}`" class="text-secondary text-sm">
+        {{ author }}
+      </NuxtLink>
+    </h3>
     <p>
       {{ body }}
     </p>
-    <div class="flex items-center gap-6 pt-4 border-t border-blue-300">
+    <footer class="flex items-center gap-6 pt-4 border-t border-blue-300">
       <FeedPostButton icon-name="lucide:thumbs-up">Like</FeedPostButton>
       <FeedPostButton icon-name="lucide:message-square-text">
         Comment
       </FeedPostButton>
       <FeedPostButton icon-name="lucide:repeat-2">Repost</FeedPostButton>
       <FeedPostButton icon-name="lucide:send">Send</FeedPostButton>
-    </div>
-  </div>
+    </footer>
+  </article>
 </template>
