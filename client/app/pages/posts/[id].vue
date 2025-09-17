c91f173f9b72013a589cdde5fd0333d041f781c7
Adam Griffin <ag@agriffin.me>
2025-09-17 15:46:20 -0700

    Refactored post detail page to use FeedPost
    Re-using the FeedPost component for post detail page. Using the newly added comments slot to display comments on the detail page by default. Comments in feed will no display by default. We will add a button to display comments there later.

--- client/app/pages/posts/[id].vue
+++ client/app/pages/posts/[id].vue
@@ -1,42 +1,34 @@
 <script lang="ts" setup>
 const route = useRoute();
 
+const postId = getRouteParam(route.params.id);
+
 const { data } = await useAsyncQuery<GetPostQuery>(GetPostDocument, {
-  id: getRouteParam(route.params.id)
+  id: postId
 } satisfies GetPostQueryVariables);
 
 const post = computed(() => data.value?.post);
-const commentEdges = computed(() => {
-  const edges = data.value?.post.postComments?.edges ?? [];
-  return edges.filter((e) => e?.node);
-});
+
+const commentEdges = computed(
+  () =>
+    data.value?.post.postComments?.edges?.filter(
+      (e): e is PostCommentEdge & { node: PostComment } => !!e && !!e.node
+    ) ?? []
+);
 </script>
 
 <template>
-  <div v-if="post" class="flex flex-col gap-6 min-w-2xs max-w-2/3 mx-auto">
-    <div>
-      <NuxtLink :to="`/users/${post.user.slug}`" class="text-secondary text-sm">
-        {{ post.user.nameWithCredentials }}
-      </NuxtLink>
-    </div>
-    <pre class="whitespace-pre-line font-sans">
-      {{ post.body }}
-    </pre>
-    <ul class="space-y-4 pl-6">
-      <li v-for="edge in commentEdges" :key="edge?.node?.id">
-        <div v-if="edge?.node?.user">
-          <NuxtLink
-            :to="`/users/${edge.node.user.slug}`"
-            class="text-secondary text-sm"
-          >
-            {{ edge.node.user.nameWithCredentials }}
-          </NuxtLink>
-        </div>
-        <p v-if="edge?.node?.body" class="py-2">
-          {{ edge.node.body }}
-        </p>
-      </li>
-    </ul>
+  <div v-if="post" class="w-140 mx-auto">
+    <FeedPost
+      :id="postId"
+      :body="post.body"
+      :author="post.user.nameWithCredentials"
+      :author-slug="post.user.slug"
+    >
+      <template v-if="commentEdges.length" #comments>
+        <PostComments :comments="commentEdges" />
+      </template>
+    </FeedPost>
   </div>
   <div v-else>
     <p>Nothing to see here</p>
