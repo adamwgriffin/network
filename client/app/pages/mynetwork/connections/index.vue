60711e535333d00c986b4ceed1fa284a1d01d1f5
Adam Griffin <ag@agriffin.me>
2025-09-18 19:19:02 -0700

    Added connections page

---
+++ client/app/pages/mynetwork/connections/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const { result } = useGetUserConnectionsQuery({
+  slug: "gregory-house"
+});
+
+const connectionEdges = computed(
+  () =>
+    result.value?.user?.connections?.edges?.filter(
+      (e): e is UserEdge & { node: User } => !!e && !!e.node
+    ) ?? []
+);
+</script>
+
+<template>
+  <div v-if="connectionEdges">
+    <h2 class="text-1xl font-bold pb-2">
+      {{ result?.user?.connectionTotal }} Connections
+    </h2>
+    <ul class="space-y-4">
+      <li v-for="{ node } in connectionEdges" :key="node.id">
+        <NuxtLink
+          :to="`/users/${node.slug}`"
+          class="flex items-center gap-2 text-secondary"
+        >
+          <UAvatar :alt="node.nameWithCredentials" />
+          <div>
+            {{ node.nameWithCredentials }}
+          </div>
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
