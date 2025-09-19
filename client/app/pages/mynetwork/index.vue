4a319b45d747a23323efa31a0ff60ae76410114c
Adam Griffin <ag@agriffin.me>
2025-09-18 19:26:06 -0700

    UI tweaks for my network

--- client/app/pages/mynetwork/index.vue
+++ client/app/pages/mynetwork/index.vue
@@ -39,22 +39,26 @@ function loadMore() {
     <h2 class="text-2xl font-bold pb-6">Connect with some cool doctors 😎</h2>
     <ul class="space-y-4">
       <li v-for="edge in result?.users.edges ?? []" :key="edge?.node?.slug">
-        <NuxtLink :to="`/users/${edge?.node?.slug}`">
-          {{ edge?.node?.nameWithCredentials }}
+        <NuxtLink
+          :to="`/users/${edge?.node?.slug}`"
+          class="flex items-center gap-2 text-secondary"
+        >
+          <UAvatar :alt="edge?.node?.nameWithCredentials" />
+          <div>
+            {{ edge?.node?.nameWithCredentials }}
+          </div>
         </NuxtLink>
       </li>
     </ul>
     <UContainer class="py-6 text-center">
-      <UTooltip text="Load more">
-        <button
-          v-if="result?.users?.pageInfo?.hasNextPage"
-          :disabled="loading"
-          class="cursor-pointer"
-          @click="loadMore"
-        >
-          <UIcon name="lucide:circle-plus" class="size-6" />
-        </button>
-      </UTooltip>
+      <button
+        v-if="result?.users?.pageInfo?.hasNextPage"
+        :disabled="loading"
+        class="cursor-pointer flex items-center gap-2"
+        @click="loadMore"
+      >
+        Give me more doctors! <UIcon name="lucide:circle-plus" class="size-6" />
+      </button>
     </UContainer>
     <p v-if="error">
       {{ error }}
