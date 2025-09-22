29282f8492f447e37dbcb3b3dd52e2cf7ea5eb83
Adam Griffin <ag@agriffin.me>
2025-09-22 15:11:08 -0700

    Added UserCard
    A card to display for users to connect with on the My Network page

---
+++ client/app/components/UserCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{ name: string; slug: string }>();
+const emit = defineEmits<{
+  (e: "connect-clicked"): void;
+}>();
+</script>
+
+<template>
+  <div
+    class="h-full flex flex-col items-center gap-y-4 rounded-lg border border-blue-300"
+  >
+    <NuxtLink :to="`/users/${slug}`" class="grow text-center px-4 pt-4">
+      <UAvatar :alt="name" class="size-24 text-5xl" />
+      <div class="font-semibold">{{ name }}</div>
+    </NuxtLink>
+    <div class="px-4 pb-4">
+      <UButton
+        color="secondary"
+        variant="outline"
+        icon="lucide:user-plus"
+        class="cursor-pointer"
+        @click="emit('connect-clicked')"
+      >
+        Connect
+      </UButton>
+    </div>
+  </div>
+</template>
