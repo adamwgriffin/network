390245d53fa70fccf283b7544bf7063eca509966
Adam Griffin <ag@agriffin.me>
2025-10-08 10:30:06 -0700

    Added test for without_connection_to scope

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,28 @@ RSpec.describe User, type: :model do
   let(:cuddy) { create(:cuddy) }
   let(:thirteen) { create(:thirteen) }
 
+  describe "#without_connection_to" do
+    it "returns users that have no connection record for the given user id" do
+      taub = create(:taub)
+      kutner = create(:kutner)
+      create(:connection, requester: house, recipient: thirteen, status: "pending")
+      create(:connection, requester: house, recipient: wilson, status: "accepted")
+      create(:connection, requester: house, recipient: cameron, status: "declined")
+
+      users = User.without_connection_to(house.id).all
+      # Queries that use this scope are expected to filter out the the user
+      # provided in the argument, i.e., house.id, if they don't want it so that
+      # the scope doesn't have to make too many assumptions, and can remain
+      # flexible
+      expect(users).to include(house)
+      expect(users).to include(taub)
+      expect(users).to include(kutner)
+      expect(users).not_to include(thirteen)
+      expect(users).not_to include(wilson)
+      expect(users).not_to include(cameron)
+    end
+  end
+
   describe "#connections" do
     it "returns all accepted connections for a user" do
       create(:connection, requester: house, recipient: wilson, status: "accepted")
